24127bc3dfe5f407d2fb5195ffb203e006e0e104
Josh Reschke <resjosh265@gmail.com>
2020-10-06 17:13:27 -0600

    rewrote cube movement which solved a few bugs and is more stable

--- Assets/scripts/Control/CubeController.cs
+++ Assets/scripts/Control/CubeController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Qubz.Enums;
+using Qubz.Movement;
 
 namespace Qubz.Control{
     public class CubeController : MonoBehaviour
     {
         public ColorEnum cubeColor;
+
+        public void DisableCubeMover(){
+            GetComponent<CubeMover>().enabled = false;
+        }
     }
 }
--- Assets/scripts/KinimaticOn.cs
+++ Assets/scripts/KinimaticOn.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
 using Qubz.Core;
+using Qubz.Control;
 
 public class KinimaticOn : MonoBehaviour {
 
 	void OnTriggerEnter(Collider col) {
+		var cubeController = col.GetComponent<CubeController>();
+		
+		if(cubeController == null) return;
+		
+		Rigidbody rigidBody = col.gameObject.GetComponent<Rigidbody>();
+		print(rigidBody);
+		rigidBody.isKinematic = false;
+		cubeController.DisableCubeMover();
+			
+			
 		//make the blocks kinematic when they reach the ground so physics do not apply
+		/*
 		if (col.tag == "Black Cube" | col.tag == "Red Cube" | col.tag == "Green Cube" | col.tag == "Normal Cube") {
 			
 			Rigidbody rb = col.GetComponent<Rigidbody> () as Rigidbody;
@@ -16,5 +28,6 @@ public class KinimaticOn : MonoBehaviour {
 			} else
 				rb.isKinematic = false;
 		}
+		*/
 	}
 }
